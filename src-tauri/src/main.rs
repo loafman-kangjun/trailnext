7d1933b6fa868e2d891a941d5433c1a9092a9fb2
Loafman <kangjun1120@hotmail.com>
2024-04-06 15:00:33 +0800

    single app upgrade

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,28 @@
 
 mod command;
 use command::basic;
+use tauri::{AppHandle, Manager};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = show_window(app);
+        }))
         .invoke_handler(tauri::generate_handler![basic::greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn show_window(app: &AppHandle) {
+    let windows = app.webview_windows();
+
+    windows
+        .values()
+        .next()
+        .expect("Sorry, no window found")
+        .set_focus()
+        .expect("Can't Bring Window to Focus");
 }
